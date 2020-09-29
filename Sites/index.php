187f86e27abc4429b38fcc0a0d0f0c29fe8a2f76
lutjens-baltazar <53823990+lutjens-baltazar@users.noreply.github.com>
2020-09-28 22:40:59 -0300

    asdasd

--- Sites/index.php
+++ Sites/index.php
@@ -21,7 +21,7 @@
   <br>
   <br>
 
-<h3 align="center"> Consulta 1: Muestre el nombre de todas las navieras </h3>
+<h3 align="center"> Consulta 2: Muestre todos los buques de la naviera ‘Francis Drake S.A.’ </h3>
 
 <form align="center" action="consultas/consulta2.php" method="post">
   <!-- Tipo:
@@ -36,7 +36,7 @@
 <br>
 <br>
 
-<h3 align="center"> Consulta 1: Muestre el nombre de todas las navieras </h3>
+<h3 align="center"> Consulta 3: Muestre todos los buques que hayan atracado en ‘Valparaiso’ el 2020 </h3>
 
 <form align="center" action="consultas/consulta3.php" method="post">
   <!-- Tipo:
@@ -51,7 +51,8 @@
 <br>
 <br>
 
-<h3 align="center"> Consulta 1: Muestre el nombre de todas las navieras </h3>
+<h3 align="center"> Consulta 4: Muestre todos los buques que hayan estado en ‘Mejillones’ al mismo tiempo que el
+buque ‘Magnolia’ </h3>
 
 <form align="center" action="consultas/consulta4.php" method="post">
   <!-- Tipo:
@@ -66,7 +67,7 @@
 <br>
 <br>
 
-<h3 align="center"> Consulta 1: Muestre el nombre de todas las navieras </h3>
+<h3 align="center"> Consulta 5: Encuentre todos los capitanes mujeres que han pasado por el puerto ‘Talcahuano’ </h3>
 
 <form align="center" action="consultas/consulta5.php" method="post">
   <!-- Tipo:
@@ -81,7 +82,7 @@
 <br>
 <br>
 
-<h3 align="center"> Consulta 1: Muestre el nombre de todas las navieras </h3>
+<h3 align="center"> Consulta 6: Encuentre el buque pesquero que tiene m´as personas trabajando </h3>
 
 <form align="center" action="consultas/consulta6.php" method="post">
   <!-- Tipo:
