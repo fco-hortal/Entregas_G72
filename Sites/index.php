5846bd133c2082731e4778eb33cf041a45c04c73
lutjens-baltazar <53823990+lutjens-baltazar@users.noreply.github.com>
2020-09-28 22:37:40 -0300

    dsda

--- Sites/index.php
+++ Sites/index.php
@@ -94,51 +94,6 @@
 </form>
 
 <br>
-<br>
-
-<h3 align="center"> Consulta 1: Muestre el nombre de todas las navieras </h3>
-
-<form align="center" action="consultas/consulta1.php" method="post">
-  <!-- Tipo:
-  <input type="text" name="tipo_elegido">
-  <br/>
-  Nombre:
-  <input type="text" name="nombre_pokemon"> -->
-  <br/><br/>
-  <input type="submit" value="Buscar">
-</form>
-
-<br>
-<br>
-
-<h3 align="center"> Consulta 1: Muestre el nombre de todas las navieras </h3>
-
-<form align="center" action="consultas/consulta1.php" method="post">
-  <!-- Tipo:
-  <input type="text" name="tipo_elegido">
-  <br/>
-  Nombre:
-  <input type="text" name="nombre_pokemon"> -->
-  <br/><br/>
-  <input type="submit" value="Buscar">
-</form>
-
-<br>
-<br>
-
-<h3 align="center"> Consulta 1: Muestre el nombre de todas las navieras </h3>
-
-<form align="center" action="consultas/consulta1.php" method="post">
-  <!-- Tipo:
-  <input type="text" name="tipo_elegido">
-  <br/>
-  Nombre:
-  <input type="text" name="nombre_pokemon"> -->
-  <br/><br/>
-  <input type="submit" value="Buscar">
-</form>
-
-<br>
 
   </form> -->
 
