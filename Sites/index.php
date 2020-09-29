7e0cef81712c00068763827987dcbcf741d4ac3f
lutjens-baltazar <53823990+lutjens-baltazar@users.noreply.github.com>
2020-09-28 22:36:01 -0300

    asdas

--- Sites/index.php
+++ Sites/index.php
@@ -20,56 +20,127 @@
   
   <br>
   <br>
-  <br>
 
-  <h3 align="center"> ¿Quieres buscar un Pokemón por su ID?</h3>
+<h3 align="center"> Consulta 1: Muestre el nombre de todas las navieras </h3>
 
-  <form align="center" action="consultas/consulta2.php" method="post">
-    Id:
-    <input type="text" name="id_elegido">
-    <br/><br/>
-    <input type="submit" value="Buscar">
-  </form>
-  
-  <br>
-  <br>
-  <br>
+<form align="center" action="consultas/consulta1.php" method="post">
+  <!-- Tipo:
+  <input type="text" name="tipo_elegido">
+  <br/>
+  Nombre:
+  <input type="text" name="nombre_pokemon"> -->
+  <br/><br/>
+  <input type="submit" value="Buscar">
+</form>
 
-  <h3 align="center"> ¿Quieres conocer los Pokemones más altos que: ?</h3>
+<br>
+<br>
 
-  <form align="center" action="consultas/consulta_altura.php" method="post">
-    Altura Mínima:
-    <input type="text" name="altura">
-    <br/><br/>
-    <input type="submit" value="Buscar">
-  </form>
-  <br>
-  <br>
-  <br>
+<h3 align="center"> Consulta 1: Muestre el nombre de todas las navieras </h3>
 
-  <h3 align="center">¿Quieres buscar todos los pokemones por tipo?</h3>
+<form align="center" action="consultas/consulta1.php" method="post">
+  <!-- Tipo:
+  <input type="text" name="tipo_elegido">
+  <br/>
+  Nombre:
+  <input type="text" name="nombre_pokemon"> -->
+  <br/><br/>
+  <input type="submit" value="Buscar">
+</form>
 
-  <?php
-  #Primero obtenemos todos los tipos de pokemones
-  require("config/conexion.php");
-  $result = $db -> prepare("SELECT DISTINCT tipo FROM pokemones;");
-  $result -> execute();
-  $dataCollected = $result -> fetchAll();
-  ?>
+<br>
+<br>
 
-  <form align="center" action="consultas/consulta1.php" method="post">
-    Seleccinar un tipo:
-    <select name="tipo">
-      <?php
-      #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
-      foreach ($dataCollected as $d) {
-        echo "<option value=$d[0]>$d[0]</option>";
-      }
-      ?>
-    </select>
-    <br><br>
-    <input type="submit" value="Buscar por tipo">
-  </form>
+<h3 align="center"> Consulta 1: Muestre el nombre de todas las navieras </h3>
+
+<form align="center" action="consultas/consulta1.php" method="post">
+  <!-- Tipo:
+  <input type="text" name="tipo_elegido">
+  <br/>
+  Nombre:
+  <input type="text" name="nombre_pokemon"> -->
+  <br/><br/>
+  <input type="submit" value="Buscar">
+</form>
+
+<br>
+<br>
+
+<h3 align="center"> Consulta 1: Muestre el nombre de todas las navieras </h3>
+
+<form align="center" action="consultas/consulta1.php" method="post">
+  <!-- Tipo:
+  <input type="text" name="tipo_elegido">
+  <br/>
+  Nombre:
+  <input type="text" name="nombre_pokemon"> -->
+  <br/><br/>
+  <input type="submit" value="Buscar">
+</form>
+
+<br>
+<br>
+
+<h3 align="center"> Consulta 1: Muestre el nombre de todas las navieras </h3>
+
+<form align="center" action="consultas/consulta1.php" method="post">
+  <!-- Tipo:
+  <input type="text" name="tipo_elegido">
+  <br/>
+  Nombre:
+  <input type="text" name="nombre_pokemon"> -->
+  <br/><br/>
+  <input type="submit" value="Buscar">
+</form>
+
+<br>
+<br>
+
+<h3 align="center"> Consulta 1: Muestre el nombre de todas las navieras </h3>
+
+<form align="center" action="consultas/consulta1.php" method="post">
+  <!-- Tipo:
+  <input type="text" name="tipo_elegido">
+  <br/>
+  Nombre:
+  <input type="text" name="nombre_pokemon"> -->
+  <br/><br/>
+  <input type="submit" value="Buscar">
+</form>
+
+<br>
+<br>
+
+<h3 align="center"> Consulta 1: Muestre el nombre de todas las navieras </h3>
+
+<form align="center" action="consultas/consulta1.php" method="post">
+  <!-- Tipo:
+  <input type="text" name="tipo_elegido">
+  <br/>
+  Nombre:
+  <input type="text" name="nombre_pokemon"> -->
+  <br/><br/>
+  <input type="submit" value="Buscar">
+</form>
+
+<br>
+<br>
+
+<h3 align="center"> Consulta 1: Muestre el nombre de todas las navieras </h3>
+
+<form align="center" action="consultas/consulta1.php" method="post">
+  <!-- Tipo:
+  <input type="text" name="tipo_elegido">
+  <br/>
+  Nombre:
+  <input type="text" name="nombre_pokemon"> -->
+  <br/><br/>
+  <input type="submit" value="Buscar">
+</form>
+
+<br>
+
+  </form> -->
 
   <br>
   <br>
