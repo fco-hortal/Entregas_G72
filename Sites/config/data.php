891ea21399df2caf18b27ec75f12149f83820f16
Sebastián Aguirre <s.aguirre@uc.cl>
2020-10-15 22:26:30 -0300

    Creacion de conexion1-php y conexion2.php, borrado conxion.php

--- Sites/config/data.php
+++ Sites/config/data.php
@@ -1,5 +1,8 @@
 <?php
-  $user = 'grupo72';
-  $password = 'wenaxd72';
-  $databaseName = 'grupo72e2';
+  $user1 = 'grupo72';
+  $password1 = 'wenaxd72';
+  $databaseName1 = 'grupo72e2';
+  $user2 = 'grupo133';
+  $password2 = 'sebajoaquin';
+  $databaseName2 = 'grupo133e2';
 ?>
