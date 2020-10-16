65abd8c0a4a2a2e11e68afd830b3374eff08de38
Sebastián Aguirre <s.aguirre@uc.cl>
2020-10-15 21:03:18 -0300

    DEMO 1.0 Página de consultas

--- Sites/index.php
+++ Sites/index.php
@@ -1,106 +1,44 @@
 <?php include('templates/header.html');   ?>
 
 <body>
-  <h1 align="center">Entrega 2 Grupo 72 </h1>
-  
+  </br>
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">GRUPO 133+72</h1>
+      <p class="lead">DEMO DEMO DEMO DEMO DEMO DEMO DEMO DEMO DEMO DEMO DEMO DEMO DEMO DEMO</p>
+    </div>
+  </div>
 
-  <br>
-
-  <h3 align="center"> Consulta 1: Muestre el nombre de todas las navieras </h3>
-
-  <form align="center" action="consultas/consulta1.php" method="post">
-    <!-- Tipo:
-    <input type="text" name="tipo_elegido">
-    <br/>
-    Nombre:
-    <input type="text" name="nombre_pokemon"> -->
-    <br/><br/>
-    <input type="submit" value="Buscar">
-  </form>
-  
-  <br>
-  <br>
-
-<h3 align="center"> Consulta 2: Muestre todos los buques de la naviera ‘Francis Drake S.A.’ </h3>
-
-<form align="center" action="consultas/consulta2.php" method="post">
-  <!-- Tipo:
-  <input type="text" name="tipo_elegido">
-  <br/>
-  Nombre:
-  <input type="text" name="nombre_pokemon"> -->
-  <br/><br/>
-  <input type="submit" value="Buscar">
-</form>
-
-<br>
-<br>
-
-<h3 align="center"> Consulta 3: Muestre todos los buques que hayan atracado en ‘Valparaiso’ el 2020 </h3>
-
-<form align="center" action="consultas/consulta3.php" method="post">
-  <!-- Tipo:
-  <input type="text" name="tipo_elegido">
-  <br/>
-  Nombre:
-  <input type="text" name="nombre_pokemon"> -->
-  <br/><br/>
-  <input type="submit" value="Buscar">
-</form>
-
-<br>
-<br>
-
-<h3 align="center"> Consulta 4: Muestre todos los buques que hayan estado en ‘Mejillones’ al mismo tiempo que el
-buque ‘Magnolia’ </h3>
-
-<form align="center" action="consultas/consulta4.php" method="post">
-  <!-- Tipo:
-  <input type="text" name="tipo_elegido">
-  <br/>
-  Nombre:
-  <input type="text" name="nombre_pokemon"> -->
-  <br/><br/>
-  <input type="submit" value="Buscar">
-</form>
-
-<br>
-<br>
-
-<h3 align="center"> Consulta 5: Encuentre todos los capitanes mujeres que han pasado por el puerto ‘Talcahuano’ </h3>
-
-<form align="center" action="consultas/consulta5.php" method="post">
-  <!-- Tipo:
-  <input type="text" name="tipo_elegido">
-  <br/>
-  Nombre:
-  <input type="text" name="nombre_pokemon"> -->
-  <br/><br/>
-  <input type="submit" value="Buscar">
-</form>
-
-<br>
-<br>
-
-<h3 align="center"> Consulta 6: Encuentre el buque pesquero que tiene más personas trabajando </h3>
-
-<form align="center" action="consultas/consulta6.php" method="post">
-  <!-- Tipo:
-  <input type="text" name="tipo_elegido">
-  <br/>
-  Nombre:
-  <input type="text" name="nombre_pokemon"> -->
-  <br/><br/>
-  <input type="submit" value="Buscar">
-</form>
+  <div class="mx-auto pl-5 pr-5 col-5">
+    <div class="card text-center">
+    <div class="card-header">
+      <ul class="nav nav-pills card-header-pills justify-content-center">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Loguear</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Registrar</a>
+        </li>
+      </ul>
+    </div>
+    <div class="card-body">
+      <form action="main.php" method="post">
+        <div class="form-group">
+          <label>Usuario</label>
+          <input type="text" class="form-control">
+        </div>
+        <div class="form-group">
+          <label>Contraseña</label>
+          <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <button type="input" class="btn btn-primary">Ingresar</button>
+      </form>
+    </div>
+  </div>
+</div>
+<!--XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX-->
 
 <br>
 
-
-
-  <br>
-  <br>
-  <br>
-  <br>
 </body>
 </html>
---
+++ Sites/main.php
@@ -0,0 +1,56 @@
+<?php include('templates/header.html');   ?>
+
+<body>
+  </br>
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">BIENVENIDO</h1>
+      <p class="lead">NOMBRE EDAD SEXO NUMERO DE PASAPORTE NACIONALIDAD</p>
+    </div>
+  </div>
+
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Mostrar info capitanes</h5>
+      <p class="card-text">123412341234</p>
+      <a href="#" class="btn btn-primary">Consultar</a>
+    </div>
+  </div>
+
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Mostrar info jefe</h5>
+      <p class="card-text">1234123412341</p>
+      <a href="#" class="btn btn-primary">Consultar</a>
+    </div>
+  </div>
+
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Cambio contraseña</h5>
+      <p class="card-text">123412341234</p>
+      <a href="#" class="btn btn-primary">Consultar</a>
+    </div>
+  </div>
+
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Mostrar navieras</h5>
+      <p class="card-text">Mostrar buques</p>
+      <a href="#" class="btn btn-primary">Consultar</a>
+    </div>
+  </div>
+
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Mostrar puertos</h5>
+      <p class="card-text">Consulta intervalo de fechas y Consulta creacion de permisos</p>
+      <a href="#" class="btn btn-primary">Consultar</a>
+    </div>
+  </div>
+
+<!--XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX-->
+  <br>
+
+</body>
+</html>
