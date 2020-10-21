b031da97a88be8c176e08a0ce23562c2c874be07
Sebastián Aguirre <s.aguirre@uc.cl>
2020-10-21 10:30:11 -0300

    Cambios a listas con botones 5.3

--- Sites/consultas/consulta_ListaNavieras.php
+++ Sites/consultas/consulta_ListaNavieras.php
@@ -40,11 +40,9 @@
                 </form>
               </td>
               <td>
-                <p>
-                  <a class=\"btn btn-primary\" data-toggle=\"collapse\" href=\"#collapseExample\" role=\"button\" aria-expanded=\"false\" aria-controls=\"collapseExample\">
-                    Link with href
-                  </a>
-                </p>
+                <button class=\"btn btn-primary\" type=\"button\" data-toggle=\"collapse\" data-target=\"#collapseExample\" aria-expanded=\"false\" aria-controls=\"collapseExample\">
+                Button with data-target
+                </button>
                 <div class=\"collapse\" id=\"collapseExample\">
                   <div class=\"card card-body\">
                     Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
