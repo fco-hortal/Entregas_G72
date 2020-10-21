b623a53ba855339c2f7f84a7e1d3629288a3318b
Sebastián Aguirre <s.aguirre@uc.cl>
2020-10-21 10:29:06 -0300

    Cambios a listas con botones 5.2

--- Sites/consultas/consulta_ListaNavieras.php
+++ Sites/consultas/consulta_ListaNavieras.php
@@ -39,6 +39,18 @@
                   <button type=\"input\" class=\"btn btn-primary\"><i class=\"far fa-eye\"></i>Ver buques de naviera</button>
                 </form>
               </td>
+              <td>
+                <p>
+                  <a class=\"btn btn-primary\" data-toggle=\"collapse\" href=\"#collapseExample\" role=\"button\" aria-expanded=\"false\" aria-controls=\"collapseExample\">
+                    Link with href
+                  </a>
+                </p>
+                <div class=\"collapse\" id=\"collapseExample\">
+                  <div class=\"card card-body\">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                  </div>
+                </div>
+                </td>
               </tr>";
             }
             ?>
