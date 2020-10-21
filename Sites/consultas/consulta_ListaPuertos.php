a582e00bb3ff22866a995f4ec7f0176ddb8d1f6e
Sebastián Aguirre <s.aguirre@uc.cl>
2020-10-20 22:52:23 -0300

    Prueba de consulta_listas 1.3

--- Sites/consultas/consulta_ListaPuertos.php
+++ Sites/consultas/consulta_ListaPuertos.php
@@ -1,15 +1,12 @@
-<?php include('../templates/header.html');   ?>
-
-<body>
 <?php
   require("../config/conexion2.php");
 
   #En caso de ser necesario se crean variables desde inputs en index ---------------------EDITAR-->
-  $var1 = $_POST["nombre_var1"];
+  #$var1 = $_POST["nombre_var1"];
 
   #Se construye la consulta SQL como un string--------------------------------------------EDITAR-->
   $query = "
-  SELECT XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX;
+  SELECT * FROM puerto;
   ";
 
   #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
@@ -19,23 +16,34 @@
   ?>
 
   <!-- Se crea tabla -->
-  <table>
-    <tr>
-      <!-- Se crea columnas de tabla -----------------------------------------------------EDITAR-->
-      <th>TITULO COLUMNA 1</th>
-      <th>TITULO COLUMNA 2</th>
-    </tr>
-  
-      <?php
-        #Se itera echo en tabla para mostrar resultados segun las columnas de la tabla ---EDITAR-->
-        foreach ($resultados as $r) {
-          echo "<tr>
-          <td>$r[0]</td>
-          <td>$r[1]</td>
-          </tr>";
-      }
-      ?>
-      
-  </table>
-
-<?php include('../templates/footer.html'); ?>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">Lista Puertos</th>
+              <th scope="col">Accion 1</th>
+              <th scope="col">Accion 2</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            #Se itera echo en tabla para mostrar resultados segun las columnas de la tabla ---EDITAR-->
+            foreach ($resultados as $r) {
+              echo "<tr>
+              <td>$r[1]</td>
+              <td>
+                <button type=\"button\" class=\"btn btn-primary\"><i class=\"far fa-eye\"></i></button>
+              </td>
+              <td>
+                <button type=\"button\" class=\"btn btn-primary\"><i class=\"far fa-eye\"></i></button>
+              </td>
+              </tr>";
+            }
+            ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
