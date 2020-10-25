2440d834456ca4e9265d6931714c7511aa34190c
Sebastián Aguirre <s.aguirre@uc.cl>
2020-10-24 23:50:26 -0300

    Implementación de funcionamiento de consultas 1.4

--- Sites/consultas/consulta_ListaPuertos.php
+++ Sites/consultas/consulta_ListaPuertos.php
@@ -43,24 +43,13 @@
               </td>
               <td>
                 <div class=\"dropdown\">
-                  <button type=\"button\" class=\"btn btn-primary dropdown-toggle\" id=\"dropdownMenu2\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\"><i class=\"far fa-eye\"></i>
-                  Registrar permiso para
+                  <button class=\"btn btn-primary dropdown-toggle\" type=\"button\" id=\"dropdownMenu2\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
+                    Dropdown
                   </button>
-
                   <div class=\"dropdown-menu\" aria-labelledby=\"dropdownMenu2\">
-
-                    <form action=\"../main/main_PuertosPermisosAstilleros.php\" method=\"post\">
-                      <input type=\"hidden\" class=\"form-control\" name=\"id\" value=\"$r[0]\">
-                      <input type=\"hidden\" class=\"form-control\" name=\"name\" value=\"$r[1]\">
-                      <button type=\"input\" class=\"btn btn-primary\"><i class=\"far fa-eye\"></i>Astillero</button>
-                    </form>  
-
-                    <form action=\"../main/main_PuertosPermisosMuelles.php\" method=\"post\">
-                      <input type=\"hidden\" class=\"form-control\" name=\"id\" value=\"$r[0]\">
-                      <input type=\"hidden\" class=\"form-control\" name=\"name\" value=\"$r[1]\">
-                      <button type=\"input\" class=\"btn btn-primary\"><i class=\"far fa-eye\"></i>Muelle</button>
-                   </form>  
-
+                    <button class=\"dropdown-item\" type=\"button\">Action</button>
+                    <button class=\"dropdown-item\" type=\"button\">Another action</button>
+                    <button class=\"dropdown-item\" type=\"button\">Something else here</button>
                   </div>
                 </div>
               </td>
