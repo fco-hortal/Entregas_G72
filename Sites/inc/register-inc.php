f2aa4ff3936cc788d3ed3d37b2603425c7342566
lutjens-baltazar <53823990+lutjens-baltazar@users.noreply.github.com>
2020-10-25 12:04:41 -0300

    Update register-inc.php

--- Sites/inc/register-inc.php
+++ Sites/inc/register-inc.php
@@ -6,10 +6,9 @@ $edad = $_POST["edad"];
 $sexo = $_POST["sexo"];
 $n_pass = $_POST["n_pas"];
 $nac = $_POST["nac"];
-$pass1 = $_POST["pass1"];
-$pass2 = $_POST["pass2"];
-echo($pass1)
-echo($pass2)
+$pass1 = $_POST["pass_1"];
+$pass2 = $_POST["pass_2"];
+
 require '../config/conexion1.php';
 require 'funciones-inc.php';
 
