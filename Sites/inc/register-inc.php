6ed5ac87ff182592d4985f19bf9966348bf5b2ca
lutjens-baltazar <53823990+lutjens-baltazar@users.noreply.github.com>
2020-10-25 11:33:42 -0300

    Update register-inc.php

--- Sites/inc/register-inc.php
+++ Sites/inc/register-inc.php
@@ -1,34 +1,33 @@
 <?php
-if (isset($_POST["submit"] )) {
 
-    $nombre = $_POST["nombre"];
-    $edad = $_POST["edad"];
-    $sexo = $_POST["sexo"];
-    $n_pass = $_POST["n_pass"];
-    $nac = $_POST["nac"];
-    $pass1 = $_POST["pass1"];
-    $pass2 = $_POST["pass2"];
-    echo($nombre);
-    require '../config/conexion1.php';
-    require 'funciones-inc.php';
 
-    if (emptyInputSignup($nombre, $edad, $sexo, $n_pass, $nac, $pass1, $pass2) !== FALSE) {
-        header('location: ../index.php');
-        exit();    
-    } 
-    if (pwdMatch($pass1, $pass2) !== FALSE) {
-        header('location: ../index.php?error=match_pwd');
-        exit();
-    }
+$nombre = $_POST["nombre"];
+$edad = $_POST["edad"];
+$sexo = $_POST["sexo"];
+$n_pass = $_POST["n_pass"];
+$nac = $_POST["nac"];
+$pass1 = $_POST["pass1"];
+$pass2 = $_POST["pass2"];
+echo($nombre);
+require '../config/conexion1.php';
+require 'funciones-inc.php';
 
-    if (uidExist($db, $n_pass) !== FALSE) {
-        header('location: ../index.php');
-        exit();
+if (emptyInputSignup($nombre, $edad, $sexo, $n_pass, $nac, $pass1, $pass2) !== FALSE) {
+    header('location: ../index.php');
+    exit();    
+} 
+if (pwdMatch($pass1, $pass2) !== FALSE) {
+    header('location: ../index.php?error=match_pwd');
+    exit();
+}
+if (uidExist($db, $n_pass) !== FALSE) {
+    header('location: ../index.php');
+    exit();
     }
     
-    userCreate($db, $nombre, $edad, $sexo, $n_pass, $nac, $pass1);
-    header('location: ../main/main_miperfil.php');
-}
+userCreate($db, $nombre, $edad, $sexo, $n_pass, $nac, $pass1);
+header('location: ../main/main_miperfil.php');
+
 
     
 ?>
