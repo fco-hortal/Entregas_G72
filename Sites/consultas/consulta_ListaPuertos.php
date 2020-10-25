bf4c067463b676f4c4b4c192085c6f0e4bb78e27
Sebastián Aguirre <s.aguirre@uc.cl>
2020-10-24 23:35:52 -0300

    Implementación de funcionamiento de consultas 1.2

--- Sites/consultas/consulta_ListaPuertos.php
+++ Sites/consultas/consulta_ListaPuertos.php
@@ -35,16 +35,25 @@
               echo "<tr>
               <td>$r[1]</td>
               <td>
-                <form action=\"../main/main_puertos_opcion1.php\" method=\"post\">
+                <form action=\"../main/main_PuertosCapacidad.php\" method=\"post\">
                   <input type=\"hidden\" class=\"form-control\" name=\"id\" value=\"$r[0]\">
                   <input type=\"hidden\" class=\"form-control\" name=\"name\" value=\"$r[1]\">
                   <button type=\"input\" class=\"btn btn-primary\"><i class=\"far fa-eye\"></i>Ver capacidad de instalaciones</button>
                 </form>
               </td>
               <td>
-                <form action=\"../main/main_puertos_opcion2.php\" method=\"post\">
+                <form action=\"../main/main_PuertosPermisos.php\" method=\"post\">
                   <input type=\"hidden\" class=\"form-control\" name=\"id\" value=\"$r[0]\">
                   <input type=\"hidden\" class=\"form-control\" name=\"name\" value=\"$r[1]\">
+
+                  <div class=\"btn-group\">
+                    <button type=\"input\" class=\"btn btn-primary dropdown-toggle\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\"><i class=\"far fa-eye\"></i>Registrar permiso para</button>
+                    <div class=\"dropdown-menu\">
+                      <a class=\"dropdown-item\" href=\"#\">Astillero</a>
+                      <a class=\"dropdown-item\" href=\"#\">Muelle</a>
+                    </div>
+                  </div>
+
                   <button type=\"input\" class=\"btn btn-primary\"><i class=\"far fa-eye\"></i>Registrar Permiso</button>
                 </form>              
               </td>
