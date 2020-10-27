0828e2a282487212cb23f9dffbea9fed1ed607c5
Francisco Hortal <francisco.hortal@uc.cl>
2020-10-27 15:29:09 -0300

    Update consulta_buscador_rut.php

--- Sites/consultas/consulta_buscador_rut.php
+++ Sites/consultas/consulta_buscador_rut.php
@@ -19,35 +19,28 @@
   ?>
 
   <!-- Se crea tabla -->
-  <div class="card pl-3 pr-3 pt-4 pb-3"s>
-      <span class="border border-primary">
-        <h5 class="card-header">Información personal:</h5>
-        <div class="card-body">
-  <table>
+
+  <table class="table table-bordered">
+    <thread>
     <tr>
       <!-- Se crea columnas de tabla -----------------------------------------------------EDITAR-->
-      <th>Nombre</th>
-      <th>Edad</th>
-      <th>Sexo</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Edad</th>
+      <th scope="col">Sexo</th>
     </tr>
+    </thead>
+    <tbody>
 
       <?php
-        #Se itera echo en tabla para mostrar resultados segun las columnas de la tabla ---EDITAR-->
         foreach ($resultados as $r) {
           echo "<tr>
-          <td>
-                <form action=\"../main/main_buscador_rut.php\" method=\"post\">
-                  <input type=\"hidden\" class=\"form-control\" name=\"id\" value=\"$r[0]\">
-                  <input type=\"hidden\" class=\"form-control\" name=\"name\" value=\"$r[1]\">
-                  <input type=\"hidden\" class=\"form-control\" name=\"name\" value=\"$r[2]\">
-                  <button type=\"input\" class=\"btn btn-primary\"><i class=\"far fa-eye\"></i>Ver buques de naviera</button>
-                </form>
-              </td>
+          <td>$r[0]</td>
+          <td>$r[1]</td>
+          <td>$r[2]</td>
           </tr>";
       }
       ?>
-      <tbody>
-
+    </tbody>
   </table>
 
 
