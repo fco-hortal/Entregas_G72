23cbb2fad200dc282a28e349c5409805c93e2f2e
joaquin-alvarez21 <53825505+joaquin-alvarez21@users.noreply.github.com>
2020-10-27 07:11:08 -0300

    Update register-inc.php

--- Sites/inc/register-inc.php
+++ Sites/inc/register-inc.php
@@ -35,8 +35,10 @@ echo "Tratando de crear el usuario";
 //userCreate($db, $nombre, $edad, $sexo, $n_pass, $nac, $pass1);
 //session_start();
 $_SESSION['pasaporte'] = $usuario_login;
+$_SESSION['pp'] = 'prueba;
 //header('location: ../main/main_miperfil.php');
 
 echo $_SESSION['pasaporte'];
- 
+
+echo $_SESSION['pp'];
 ?>
