3ea7780a49a4ee28b6986199a564700f9da237df
joaquin-alvarez21 <53825505+joaquin-alvarez21@users.noreply.github.com>
2020-10-27 06:42:32 -0300

    Update funciones-inc.php

--- Sites/inc/funciones-inc.php
+++ Sites/inc/funciones-inc.php
@@ -46,9 +46,9 @@ function uidExist($db, $n_pass) {
 
 }
 
-function userCreate($db, $nombre, $edad, $sexo, $n_pass, $nac, $pass1, $id = 1) {
+function userCreate($db, $nombre, $edad, $sexo, $n_pass, $nac, $pass1) {
     
-    $sql = "INSERT INTO usuarios (nombre, edad, sexo, pasaporte, nacionalidad, contraseña, id) VALUES ($nombre, $edad, $sexo,$n_pass, $nac, $pass1, $id)";
+    $sql = "INSERT INTO usuarios (nombre, edad, sexo, pasaporte, nacionalidad, contraseña, id) VALUES ($nombre, $edad, $sexo,$n_pass, $nac, $pass1, '1')";
     $results = $db -> prepare($sql);    
     $results -> execute();
     echo "Tratando de crear el usuario";
