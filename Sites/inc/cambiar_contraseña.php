73cbcc6fdfd53527ea9942c730609819b3b1861b
lutjens-baltazar <53823990+lutjens-baltazar@users.noreply.github.com>
2020-10-27 16:39:14 -0300

    sdad

--- Sites/inc/cambiar_contraseña.php
+++ Sites/inc/cambiar_contraseña.php
@@ -2,7 +2,28 @@
 require "../config/conexion1.php";
 require  "funciones_contraseña.php";
 
-$
+$pass_act = $_POST['pass_old'];
+$pass_new1 = $_POST['pass_new1'];
+$pass_new2 = $_POST['pass_new2'];
+$pasaporte = $_SESSION['pasaporte'];
+
+if (passExist($pass_act, $pasaporte, $db) == TRUE) {
+    header('location: ../main/main_miperfil.php?error=wrongPassword');
+    exit();
+
+
+}
+
+if ($pass_new1 != $pass_new2) {
+    header('location: ../main/main_miperfil.php?error=passwordDontMatch');
+    exit();
+}
+
+#cambio contraseña
+
+header('location: ../main/main_miperfil.php?error=wrongPassword?PasswordChange');
+
+
 
 
 
