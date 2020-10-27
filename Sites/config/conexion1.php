66b6b38827d1f7b6682a398eefaac0d583bd697a
joaquin-alvarez21 <53825505+joaquin-alvarez21@users.noreply.github.com>
2020-10-27 06:28:26 -0300

    Update conexion1.php

--- Sites/config/conexion1.php
+++ Sites/config/conexion1.php
@@ -3,7 +3,7 @@
     #Pide las variables para conectarse a la base de datos.
     require('data.php'); 
     # Se crea la instancia de PDO
-    $db = new PDO("pgsql:dbname=$databaseName1;host=localhost;port=5432;user=$user1;password=$password1", array(PDO::ATTR_PERSISTENT => true));
+    $db = new PDO("pgsql:dbname=$databaseName1;host=localhost;port=5432" $user1, $password1, array(PDO::ATTR_PERSISTENT => true));
   } catch (Exception $e) {
     echo "No se pudo conectar a la base de datos: $e";
   }
