f5e16921dfa0a17d45ba09920e9d534c513d0510
joaquin-alvarez21 <53825505+joaquin-alvarez21@users.noreply.github.com>
2020-10-27 06:39:51 -0300

    Update register-inc.php

--- Sites/inc/register-inc.php
+++ Sites/inc/register-inc.php
@@ -26,7 +26,7 @@ if (uidExist($db, $n_pass) == TRUE) {
     exit();
     }
 $sql = "INSERT INTO algo (nombre, edad, sexo, pasaporte, nacionalidad, contraseña, id) VALUES ($nombre, $edad, $sexo,$n_pass, $nac, $pass1, $id)";
-$results = $db->prepare($sql);    
+$results = $db->prepare("INSERT INTO algo (nombre, edad, sexo, pasaporte, nacionalidad, contraseña, id) VALUES ($nombre, $edad, $sexo,$n_pass, $nac, $pass1, $id)");    
 $results->execute();
 echo "Tratando de crear el usuario";
 /*userCreate($db, $nombre, $edad, $sexo, $n_pass, $nac, $pass1);
