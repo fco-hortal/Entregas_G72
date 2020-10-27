fb09f855c6034333e920897aa1b8515720876c4f
joaquin-alvarez21 <53825505+joaquin-alvarez21@users.noreply.github.com>
2020-10-27 06:57:18 -0300

    Update register-inc.php

--- Sites/inc/register-inc.php
+++ Sites/inc/register-inc.php
@@ -12,9 +12,6 @@ $pass2 = $_POST["pass_2"];
 require '../config/conexion1.php';
 require 'funciones-inc.php';
 
-$results = $db->prepare("INSERT INTO algo (nombre, edad, sexo, pasaporte, nacionalidad, contraseña, id) VALUES ($nombre, $edad, $sexo,$n_pass, $nac, $pass1, '1');");    
-$results->execute();
-
 if (emptyInputSignup($nombre, $edad, $sexo, $n_pass, $nac, $pass1, $pass2) !== FALSE) {
     header('location: ../index_register.php?error=emptyinput');
     exit();    
@@ -28,12 +25,17 @@ if (uidExist($db, $n_pass) == TRUE) {
 
     exit();
     }
+
 $sql = "INSERT INTO algo (nombre, edad, sexo, pasaporte, nacionalidad, contraseña, id) VALUES ($nombre, $edad, $sexo,$n_pass, $nac, $pass1, '1')";
+$results = $db->prepare($sql);    
+$results->execute();
+
 echo "Tratando de crear el usuario";
-/*userCreate($db, $nombre, $edad, $sexo, $n_pass, $nac, $pass1);
+//userCreate($db, $nombre, $edad, $sexo, $n_pass, $nac, $pass1);
 session_start();
 $_SESSION['pasaporte'] = $usuario_login;
-header('location: ../main/main_miperfil.php');*/
+//header('location: ../main/main_miperfil.php');
 
+echo $_SESSION
  
 ?>
